226435a0d94ba21de23e21d27244be6232965c41
Daniel Edwards <95729902+datboyblu3@users.noreply.github.com>
2024-11-03 14:14:51 -0500

    Update network.tf

--- Amazon AWS/network.tf
+++ Amazon AWS/network.tf
@@ -1,4 +1,9 @@
 # Create a VPC
-resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
+resource "aws_vpc" "tmp_vpc" {
+  cidr_block = "10.123.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support = true
+
+tags = {
+  Name = "dev"
 }
