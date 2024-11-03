8493f1e21e79ddc917f7ee9d3b372cfba6ec0e39
Daniel Edwards <95729902+datboyblu3@users.noreply.github.com>
2024-11-03 14:04:06 -0500

    Update providers.tf

--- Amazon AWS/providers.tf
+++ Amazon AWS/providers.tf
@@ -10,9 +10,18 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
+  shared_config_files      = ["/Users/tf_user/.aws/conf"]
+  shared_credentials_files = ["/Users/tf_user/.aws/creds"]
+  profile                  = "customprofile"
 }
 
 # Create a VPC
 resource "aws_vpc" "example" {
   cidr_block = "10.0.0.0/16"
+}
+
+provider "aws" {
+  shared_config_files      = ["/Users/tf_user/.aws/conf"]
+  shared_credentials_files = ["/Users/tf_user/.aws/creds"]
+  profile                  = "customprofile"
 }
