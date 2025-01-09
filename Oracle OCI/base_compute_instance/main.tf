3e82e28bc435e0c14e784c5ddbd3284d52d91dc0
Daniel Edwards <95729902+datboyblu3@users.noreply.github.com>
2025-01-09 18:45:55 -0500

    Update main.tf

--- Oracle OCI/base_compute_instance/main.tf
+++ Oracle OCI/base_compute_instance/main.tf
@@ -1,23 +1,23 @@
 resource "oci_core_vcn" "security_lab_vcn" {
-  cidr_block            = var.vcn_cidr_block
-  display_name          = "security-lab-vcn"
-  compartment_id        = var.compartment
+  cidr_block     = var.vcn_cidr_block
+  display_name   = "security-lab-vcn"
+  compartment_id = var.compartment
 }
 
 resource "oci_core_internet_gateway" "security_lab_igw" {
-  display_name          = var.gateway_name
-  vcn_id                = oci_core_vcn.security_lab_vcn.id
-  compartment_id        = var.compartment
+  display_name   = var.gateway_name
+  vcn_id         = oci_core_vcn.security_lab_vcn.id
+  compartment_id = var.compartment
 }
 
 resource "oci_core_route_table" "security_lab_route_table" {
-  vcn_id                = oci_core_vcn.security_lab_vcn.id
-  compartment_id        = var.compartment
+  vcn_id         = oci_core_vcn.security_lab_vcn.id
+  compartment_id = var.compartment
 
   route_rules {
-      cidr_block        = var.route_table_cidr
-      network_entity_id = oci_core_internet_gateway.security_lab_igw.id
-    }
+    destination        = var.route_table_cidr
+    network_entity_id = oci_core_internet_gateway.security_lab_igw.id
+  }
 }
 
 resource "oci_core_subnet" "security_lab_public_subnet" {
@@ -30,11 +30,11 @@ resource "oci_core_subnet" "security_lab_public_subnet" {
 }
 
 resource "oci_core_subnet" "security_lab_private_subnet" {
-  vcn_id                      = oci_core_vcn.security_lab_vcn.id
-  cidr_block                  = var.private_cidr_block
-  display_name                = "security-lab-private-subnet"
-  compartment_id              = var.compartment
-  route_table_id              = oci_core_route_table.security_lab_route_table.id
+  vcn_id         = oci_core_vcn.security_lab_vcn.id
+  cidr_block     = var.private_cidr_block
+  display_name   = "security-lab-private-subnet"
+  compartment_id = var.compartment
+  route_table_id = oci_core_route_table.security_lab_route_table.id
 }
 
 resource "oci_core_security_list" "security_lab_fw" {
@@ -43,25 +43,25 @@ resource "oci_core_security_list" "security_lab_fw" {
   vcn_id         = oci_core_vcn.security_lab_vcn.id
 
   egress_security_rules {
-      protocol    = "all"
-      destination = "0.0.0.0/0"
-    }
-  
+    protocol    = "all"
+    destination = "0.0.0.0/0"
+  }
+
   ingress_security_rules {
-      protocol      = "6" # TCP
-      source        = "0.0.0.0/0"
-      tcp_options {
-         min = 22
-         max = 22
-      }
+    protocol = "6" # TCP
+    source   = "0.0.0.0/0"
+    tcp_options {
+      min = 22
+      max = 22
     }
+  }
 }
 
 resource "oci_core_instance" "security_lab_instance" {
   availability_domain = data.oci_identity_availability_domains.security_lab_ad.availability_domains[0].name
   compartment_id      = var.compartment
   display_name        = "security-lab-instance"
-  shape               = "VM.Standard2.4" # Choose your shape
+  shape               = "VM.Standard2.8" # Choose your shape
 
   create_vnic_details {
     subnet_id        = oci_core_subnet.security_lab_private_subnet.id
@@ -70,19 +70,38 @@ resource "oci_core_instance" "security_lab_instance" {
 
   metadata = {
     ssh_authorized_keys = file("~/.ssh/oci_key.pub") # Path to your public key
+    user_data           = base64encode(<<-EOT
+      #!/bin/bash
+      # Install Ansible
+      # yum update -y
+      # yum install -y ansible
+    EOT
+    )
   }
 
   source_details {
     source_type = "image"
     source_id   = var.source_id
   }
-}
 
-data "oci_identity_availability_domains" "security_lab_ad" {
-  compartment_id = var.tenancy_ocid
-}
+  provisioner "remote-exec" {
+    inline = ["sudo dnf install -y python3"]
+
+    connection {
+      host        = self.public_ip
+      type        = "ssh"
+      user        = "opc"
+      private_key = file("~/.ssh/oci_key")
+    }
+  }
 
  provisioner "local-exec" {
     working_dir = var.dir
     command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u opc -i '${self.public_ip},' --private-key '~/.ssh/oci_key' -e '~/.ssh/oci_key.pub' create_user.yml"
   }
+
+}
+
+data "oci_identity_availability_domains" "security_lab_ad" {
+  compartment_id = var.tenancy_ocid
+}
