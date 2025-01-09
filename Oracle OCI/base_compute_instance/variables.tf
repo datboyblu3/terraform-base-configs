5c31513085320591c65de05147c809944c6d1c76
Daniel Edwards <95729902+datboyblu3@users.noreply.github.com>
2025-01-09 18:47:07 -0500

    Update variables.tf

--- Oracle OCI/base_compute_instance/variables.tf
+++ Oracle OCI/base_compute_instance/variables.tf
@@ -1,60 +1,66 @@
 variable "tenancy_ocid" {
-  type = string
+  type        = string
   description = "OCI tenancy identifier"
 }
 
 variable "user_ocid" {
-  type = string
+  type        = string
   description = "OCI user identifier"
 }
 
 variable "fingerprint" {
-  type = string
+  type        = string
   description = "OCI fingerprint for the key pair"
 }
 
 variable "region" {
-  type = string
+  type        = string
   description = "OCI region"
 }
 
 
 variable "compartment" {
-  type = string
+  type        = string
   description = "Compartment to test proof of concepts"
 }
 
 variable "public_cidr_block" {
-  type = string
+  type        = string
   description = "Public IPV4 address"
 }
 
 variable "private_cidr_block" {
-  type = string
+  type        = string
   description = "Private IPV4 address"
 }
 
 variable "vcn_cidr_block" {
-  type = string
+  type        = string
   description = "IP subnet range of VCN"
 }
 
 variable "vcn_name" {
-  type = string
+  type        = string
   description = "Virtual cloud network name"
 }
 
 variable "gateway_name" {
-  type = string
+  type        = string
   description = "Internet gateway name"
 }
 
 variable "route_table_cidr" {
-  type = string
+  type        = string
   description = "Internet gateway name"
 }
 
 variable "source_id" {
-  type = string
+  type        = string
   description = "Source id image of compute instance"
 }
+
+variable "pub_key" {}
+
+variable "pvt_key" {}
+
+variable "dir" {}
